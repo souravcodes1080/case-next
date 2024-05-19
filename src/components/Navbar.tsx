b22dcca6a888afe62f00a334c31f2f709f5ca9d0
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-05-19 12:16:39 +0530

    navbar logo changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href={"/"} className="flex z-40 font-semibold">
-            case<span className="text-green-600">python</span>
+            case<span className="text-green-600">Dev</span>
           </Link>
           <div className="h-full flex items-center space-x-4">
             {user ? (
